daa16361e08ff453490ea6d258086ebd7f0c705d
Tanmay Sinha <tanmay3011@gmail.com>
2017-08-24 12:43:09 +0530

    Fix bug related to the already applied multi-coupon promotion.  Fix bug related to the blank promotion code application after multi-coupon promotion applied.  Fix bug related to the applying of not eligible promotion.  Show correct error messages wrt the cases related to existing promotion.

--- app/models/spree/promotion_decorator.rb
+++ app/models/spree/promotion_decorator.rb
@@ -2,10 +2,14 @@ Spree::Promotion.class_eval do
   has_many :codes, class_name: Spree::Promotion::Code, dependent: :destroy
   has_many :promotable_users, through: :codes, source: :user
 
-  def self.with_coupon_code(coupon_code)
+  def self.with_coupon_code(coupon_code, user = nil)
     code = coupon_code.to_s.strip.downcase
-    joins('LEFT JOIN spree_promotion_codes ON spree_promotions.id = spree_promotion_codes.promotion_id')
-    .find_by("(lower(spree_promotions.code) = ? AND spree_promotions.multi_coupon = ?) OR (lower(spree_promotion_codes.code) = ? AND spree_promotions.multi_coupon = ?)", code, false, code, true)
+    promotions = joins('LEFT JOIN spree_promotion_codes ON spree_promotions.id = spree_promotion_codes.promotion_id')
+    if user
+      promotions.find_by("(lower(spree_promotions.code) = ? AND spree_promotions.multi_coupon = ?) OR (lower(spree_promotion_codes.code) = ? AND spree_promotions.multi_coupon = ? AND spree_promotion_codes.user_id = ?)", code, false, code, true, user.id)
+    else
+      promotions.find_by("(lower(spree_promotions.code) = ? AND spree_promotions.multi_coupon = ?) OR (lower(spree_promotion_codes.code) = ? AND spree_promotions.multi_coupon = ?)", code, false, code, true)
+    end
   end
 
   def eligible?(promotable)
@@ -21,8 +25,18 @@ Spree::Promotion.class_eval do
     end
 
     def authorized?(promotable)
-      promotable_user = promotable.is_a?(Spree::Order) ? promotable.user : promotable.order.user
-      already_included_in_order = promotable.is_a?(Spree::Order) ? promotable.promotions.include?(self) : promotable.order.promotions.include?(self)
-      promotable_user && promotable_users.include?(promotable_user) && (!codes.where(user: promotable_user, used: true).exists? || already_included_in_order)
+      promotable_order = promotable.is_a?(Spree::Order) ? promotable : promotable.order
+      promotable_user, coupon_code = promotable_order.user, promotable_order.coupon_code
+      already_included_in_order = promotable_order.promotions.include?(self) || promotable_order.all_adjustments.eligible.any? { |adjustment| adjustment.source.promotion == self }
+
+      if coupon_code.blank? || codes.where("user_id = ? AND lower(code) = ?", promotable_user.id, coupon_code).exists?
+        if already_included_in_order
+          promotable_user && promotable_users.include?(promotable_user)
+        else
+          promotable_user && promotable_users.include?(promotable_user) && !codes.where(user: promotable_user, used: true).exists?
+        end
+      else
+        false
+      end
     end
 end
